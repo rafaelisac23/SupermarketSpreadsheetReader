f332682fe1a9b84396f4f83e97390c0f53b25d3d
rafaelisac23 <rafael.darrigo@gmail.com>
2026-06-23 13:16:13 -0300

    Add_database_cv_and_create_model

--- Models/Menu.cs
+++ Models/Menu.cs
@@ -5,19 +5,37 @@ public static class Menu
 
     public static void ShowMenuBorders()
     {
-        for (int i = 0; i < 30; i++)
+        Console.Clear();
+
+        int width = 80;
+        int height = 23;
+
+        string horizontal = new string('═', width - 2);
+        
+        Console.WriteLine("╔" + horizontal + "╗");
+        
+        for (int i = 0; i < height - 2; i++)
         {
-            Console.Write("-");
+            Console.WriteLine("║" + new string(' ', width - 2) + "║");
         }
+
+        Console.WriteLine("╚" + horizontal + "╝");
     }
 
     public static void ShowMainMenu()
     {
         Console.Clear();
         ShowMenuBorders();
+        ShowOptions();
+    }
 
+    public static void ShowOptions()
+    {
+        int option = 0;
+        
+        Console.SetCursorPosition(2,2);
+        Console.WriteLine("aa");
         Console.ReadLine();
-
     }
     
     
